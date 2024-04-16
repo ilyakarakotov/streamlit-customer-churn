1df68a41ac3d2ddfe66c571e710b6117ddc0c429
Ilya Karakotov <138952471+ilyakarakotov@users.noreply.github.com>
2024-04-15 18:58:03 -0700

    Improved syntax in app.py

--- model/app.py
+++ model/app.py
@@ -1,11 +1,10 @@
 import pandas as pd
-
+import pickle as pickle
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
-import pickle as pickle
 
 
 def create_model(data):
